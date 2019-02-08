12d1d0129d36a8ba5ed7f988f77862df821167b8
Kimiaki Kuno <knokmki612@gmail.com>
2019-02-08 09:50:37 +0900

    Add purgecss-webpack-plugin

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,22 @@
+const PurgecssPlugin = require('purgecss-webpack-plugin')
+const glob = require('glob-all')
+
 module.exports = {
   lintOnSave: false,
+
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      return {
+        plugins: [
+          new PurgecssPlugin({
+            paths: glob.sync([
+              './src/**/*.vue'
+            ])
+          })
+        ]
+      }
+    }
+  },
 
   pluginOptions: {
     i18n: {
