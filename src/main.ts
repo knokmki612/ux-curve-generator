060104269e513d7e202f2539205fa80538183137
Kimiaki Kuno <knokmki612@gmail.com>
2019-02-26 18:13:05 +0900

    Add custom component classes structure of tailwind.css

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import i18n from './i18n'
-import './assets/tailwind.css'
+import './assets/tailwind.sass'
 import store from './store'
 
 Vue.config.productionTip = false
