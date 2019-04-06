9e026312147aa02d0ad0d29477e0a2e85a5958ba
Kimiaki Kuno <knokmki612@gmail.com>
2019-04-06 19:48:37 +0900

    Adjust PurgeCSS paths

--- vue.config.js
+++ vue.config.js
@@ -16,6 +16,7 @@ module.exports = {
         plugins: [
           new PurgecssPlugin({
             paths: glob.sync([
+              './public/*.html',
               './src/**/*.vue'
             ]),
             extractors: [
