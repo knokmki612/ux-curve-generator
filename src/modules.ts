91e622c9b3027852d828af3d1bc0a9b501813ccb
Kimiaki Kuno <knokmki612@gmail.com>
2019-09-15 15:19:30 +0900

    Add vuex-presist and vuex-module-decorators

---
+++ src/modules.ts
@@ -0,0 +1,98 @@
+import { Module, VuexModule, Mutation } from 'vuex-module-decorators'
+import { isAfter } from 'date-fns'
+import { FixedUxEvent, UxEvent } from './types'
+
+
+type UxEventFragment = {
+  score?: string | number
+  description?: string
+  date?: Date
+}
+
+function filter(value: UxEventFragment): UxEventFragment {
+  const isString = (score: string | number | undefined): score is string =>
+    typeof score === 'string'
+  if (!isString(value.score)) return value
+  const score = parseInt(value.score, 10)
+  return {
+    score: isNaN(score) ? 0
+      : score > 100 ? 100
+      : score < -100 ? -100
+      : score
+  }
+}
+
+@Module
+export class ExpectedUx extends VuexModule {
+  expectedUx: FixedUxEvent = {
+    score: 0,
+    description: ''
+  }
+
+  @Mutation
+  updateExpectedUx(payload: UxEventFragment) {
+    this.expectedUx = Object.assign(
+      this.expectedUx,
+      filter(payload)
+    )
+  }
+}
+
+@Module
+export class ActualUx extends VuexModule {
+  actualUx: UxEvent = {
+    score: 0,
+    description: '',
+    get date() { return new Date() }
+  }
+
+  @Mutation
+  updateActualUx(payload: UxEventFragment) {
+    this.actualUx = Object.assign(
+      this.actualUx,
+      filter(payload)
+    )
+  }
+}
+
+@Module
+export class UxEvents extends VuexModule {
+  uxEvents: Array<UxEvent> = []
+
+  @Mutation
+  addUxEvent(payload: UxEvent) {
+    this.uxEvents.push(payload)
+    this.uxEvents.sort((a, b) => {
+      return (isAfter(a.date, b.date)) ? 1 : -1
+    })
+  }
+
+  @Mutation
+  updateUxEvent(payload: { key: number, value: UxEventFragment }) {
+    const { key, value } = payload
+    const uxEvent: UxEvent = Object.assign(
+      this.uxEvents[key],
+      filter(value)
+    )
+    this.uxEvents.splice(key, 1 , uxEvent)
+    this.uxEvents.sort((a, b) => {
+      return (isAfter(a.date, b.date)) ? 1 : -1
+    })
+  }
+
+  @Mutation
+  deleteUxEvent(payload: { key: number }) {
+    const { key } = payload
+    this.uxEvents.splice(key, 1)
+  }
+}
+
+@Module
+export class LastChosenUnitKey extends VuexModule {
+  lastChosenUnitKey: string = 'year'
+
+  @Mutation
+  updateUnitKey(payload: string) {
+    this.lastChosenUnitKey = payload
+  }
+}
--- src/store.ts
+++ src/store.ts
@@ -1,76 +1,18 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import { isAfter } from 'date-fns'
-import { FixedUxEvent, UxEvent } from './types'
+import VuexPersist from 'vuex-persist'
+import { ExpectedUx, ActualUx, UxEvents, LastChosenUnitKey } from './modules'
+
+const vuexLocal = new VuexPersist()
 
 Vue.use(Vuex)
 
-type UxEventFragment = {
-  score?: string | number
-  description?: string
-  date?: Date
-}
-
-function filter(value: UxEventFragment): UxEventFragment {
-  const isString = (score: string | number | undefined): score is string =>
-    typeof score === 'string'
-  if (!isString(value.score)) return value
-  const score = parseInt(value.score, 10)
-  return {
-    score: isNaN(score) ? 0
-      : score > 100 ? 100
-      : score < -100 ? -100
-      : score
-  }
-}
-
 export default new Vuex.Store({
-  state: {
-    expectedUx: { score: 0, description: '' } as FixedUxEvent,
-    actualUx: { score: 0, description: '',
-      get date() { return new Date() } } as UxEvent,
-    uxEvents: [] as Array<UxEvent>,
-    lastChosenUnitKey: 'year'
+  modules: {
+    ActualUx,
+    ExpectedUx,
+    UxEvents,
+    LastChosenUnitKey
   },
-  mutations: {
-    updateExpectedUx(state, payload: UxEventFragment) {
-      state.expectedUx = Object.assign(
-        state.expectedUx,
-        filter(payload)
-      )
-    },
-    updateActualUx(state, payload: UxEventFragment) {
-      state.actualUx = Object.assign(
-        state.actualUx,
-        filter(payload)
-      )
-    },
-    addUxEvent(state, payload: UxEvent) {
-      state.uxEvents.push(payload)
-      state.uxEvents.sort((a, b) => {
-        return (isAfter(a.date, b.date)) ? 1 : -1
-      })
-    },
-    updateUxEvent(state, payload: { key: number, value: UxEventFragment }) {
-      const { key, value } = payload
-      const uxEvent: UxEvent = Object.assign(
-        state.uxEvents[key],
-        filter(value)
-      )
-      state.uxEvents.splice(key, 1 , uxEvent)
-      state.uxEvents.sort((a, b) => {
-        return (isAfter(a.date, b.date)) ? 1 : -1
-      })
-    },
-    deleteUxEvent(state, payload: { key: number }) {
-      const { key } = payload
-      state.uxEvents.splice(key, 1)
-    },
-    updateUnitKey(state, payload: string) {
-      state.lastChosenUnitKey = payload
-    }
-  },
-  actions: {
-
-  }
+  plugins: [vuexLocal.plugin]
 })
