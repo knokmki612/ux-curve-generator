aa4e3496d0bcb46e0d91bfa5582a00cdca841d4d
Kimiaki Kuno <knokmki612@gmail.com>
2019-09-23 20:29:04 +0900

    Add UxCurveTitle
    Fix forgotten git-add

---
+++ src/components/UxCurveTitle.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="ux-curve-title text-center">
+    <i18n
+      path="UxCurveTitle.title"
+      tag="h2"
+      class="leading-normal"
+    >
+      <p
+        v-if="readonly"
+        slot="subject"
+        class="inline-block form text-xl"
+      >
+        {{ subject }}
+      </p>
+      <input
+        v-else
+        slot="subject"
+        class="form max-w-full lg:w-1/2 text-xl text-center"
+        :value="subject"
+        @input="updateSubject($event.target.value)"
+      >
+      <span
+        slot="uxCurve"
+        class="whitespace-no-wrap"
+      >{{ $t('UxCurveTitle.uxCurve') }}</span>
+    </i18n>
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator'
+import { mapMutations } from 'vuex'
+
+@Component({
+  methods: {
+    ...mapMutations('Subject', ['updateSubject'])
+  }
+})
+export default class UxCurveTitle extends Vue {
+  @Prop(Boolean) readonly readonly!: boolean
+  @Prop(String) readonly subject!: string
+}
+</script>
+
+<style scoped lang="sass">
+.ux-curve-title
+  @apply rounded shadow-md px-6 py-4 bg-gray-300
+</style>
